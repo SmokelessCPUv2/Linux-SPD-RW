2332b2fac7cb8aedbbd81ecfe7eddfb63e71e1e9
SmokelessCPUv2 <110113863+SmokelessCPUv2@users.noreply.github.com>
2022-10-30 15:19:39 +0100

    Fixed Error formatting

--- spd_write.c
+++ spd_write.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
         {
             if(i2c_smbus_write_byte_data(file, i, buf[i])<0)
            {
-            printf("Failded to write address Page 0 Add %02x",i);
+            printf("Failded to write Page 0 Address: %02x\n",i);
            }
 
            usleep(8000);
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
         {
             if(i2c_smbus_write_byte_data(file, i, buf[i+256])<0)
            {
-            printf("Failded to write address Page 1 Add %02x",i);
+            printf("Failded to write Page 1 Address: %02x\n",i);
            }
 
            usleep(8000);
