62860db6fbf9d49e1f2ee84b8d36cdc59e50afb2
SmokelessCPUv2 <110113863+SmokelessCPUv2@users.noreply.github.com>
2022-10-30 14:54:56 +0100

    Added spd_write_utility

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 CC=gcc
 LIBS= -li2c
 
-spd_dump: main.o
-	$(CC) -o spd_dump main.o $(LIBS)
+spd_dump: spd_dump.o
+	$(CC) -o spd_dump spd_dump.o $(LIBS)
+
+spd_write: spd_write.o
+	$(CC) -o spd_write spd_write.o $(LIBS)
---
+++ spd_write.c
@@ -0,0 +1,68 @@
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <i2c/smbus.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Run as %s i2c-num device address in-file\n",argv[0]);
+        printf("Run as %s 1 0x50 spd_dump.bin\n",argv[0]);
+    }
+    else
+    {
+        int spd = open(argv[3], O_RDONLY);
+        if (spd < 1)
+        {
+            perror("Failed to open file");
+            exit(1);
+        }
+        read(spd, buf, 512);
+        close(spd);
+
+        int i2c_num = strtol(argv[1],NULL,0);
+        int addr = strtol(argv[2],NULL,16);
+        int file;
+        system("modprobe i2c-dev");
+        char buf[512] = {0};
+        char filename[64];
+        sprintf(filename,"/dev/i2c-%d",i2c_num);
+        if ((file = open(filename, O_RDWR)) < 0)
+        {
+            perror("Failed to open the i2c bus");
+            exit(1);
+        }
+
+        //Set Page 0 (SPA0 command)
+        ioctl(file, I2C_SLAVE, 0x36);
+        i2c_smbus_write_byte_data(file,0,0);
+
+        //Go back to regular to perform reading
+        ioctl(file, I2C_SLAVE, addr);
+
+        //Read the entire page 0
+        for (int i = 0; i < 256; i++)
+        {
+           i2c_smbus_write_byte_data(file, i, buf[i]);
+        }
+
+        //Set Page 1 (SPA1 command)
+        ioctl(file, I2C_SLAVE, 0x37);
+        i2c_smbus_write_byte_data(file,0,0);
+        ioctl(file, I2C_SLAVE, addr);
+
+        //Read the entire page 1
+        for (int i = 0; i < 256; i++)
+        {
+            i2c_smbus_write_byte_data(file, i, buf[i+256]);
+        }
+
+    }
+}
